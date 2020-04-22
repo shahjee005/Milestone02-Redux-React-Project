138b860b536ab42af43ebf445cd3d185c3673ef5
Your Namenbehina <nbehina@gmail.com>
2020-04-22 11:08:42 -0600

    Chenge in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1>Milestone-02 Redux-react-typescript Projet  </h1>
+      <h1>Milestone-02 Redux-react-typescript Projet </h1>
     </div>
   );
 }
