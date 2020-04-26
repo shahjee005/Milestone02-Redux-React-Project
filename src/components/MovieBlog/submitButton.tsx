d17db2f3bcaafdcbd2b3db8ee3070c9ec56f3d63
Syed-Gardezi <61136718+shahjee005@users.noreply.github.com>
2020-04-25 18:22:21 -0600

    submit button

--- src/components/MovieBlog/submitButton.tsx
+++ src/components/MovieBlog/submitButton.tsx
@@ -1,1 +1,17 @@
+import * as React from 'react';
+import { BlogPost } from '../../store/blogPost/types';
 
+export interface ISubmitButtonProps {
+  blogPosts: BlogPost[];
+  blogPostDescription: string;
+}
+
+export class SubmitButton extends React.Component<ISubmitButtonProps> {
+  public render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
